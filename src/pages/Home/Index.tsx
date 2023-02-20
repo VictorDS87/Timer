657a0a1105efce275f66f80789aa8335a12bac86
Victor donizete <victordonizete65@gmail.com>
2023-02-19 23:03:37 -0300

    two bug in timer fixed

--- src/pages/Home/Index.tsx
+++ src/pages/Home/Index.tsx
@@ -39,12 +39,18 @@ export function Home() {
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
   useEffect(() => {
+    let interval: number
+
     if (activeCycle) {
-      setInterval(() => {
+      interval = setInterval(() => {
         setAmountSecondsPassed(
           differenceInSeconds(new Date(), activeCycle.startDate),
         )
       }, 1000)
+    }
+
+    return () => {
+      clearInterval(interval)
     }
   }, [activeCycle])
 
@@ -61,6 +67,7 @@ export function Home() {
     setCycles((state) => [...state, newCycle])
     setActiveCycleId(id)
 
+    setAmountSecondsPassed(0)
     reset()
   }
 
@@ -72,6 +79,10 @@ export function Home() {
 
   const minutes = String(minutesAmount).padStart(2, '0')
   const seconds = String(secondsAmount).padStart(2, '0')
+
+  useEffect(() => {
+    document.title = `${minutes}:${seconds}`
+  }, [minutes, seconds])
 
   const task = watch('task')
 
